eb26b97c950875ee8ddc1c24e037936d42817065
mcworkaholic <wevans302@gmail.com>
2023-03-21 03:03:42 -0500

    modified sql creation script

--- Music Player/Scripts/create.sql
+++ Music Player/Scripts/create.sql
@@ -1,21 +1,21 @@
 CREATE TABLE user (
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
+  username VARCHAR(50) UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
 
 CREATE TABLE playlist (
   playlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
-  playlist_name TEXT NOT NULL UNIQUE, 
+  playlist_name VARCHAR(50) NOT NULL UNIQUE, 
   FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
 
 CREATE TABLE song (
   song_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  song_title TEXT NOT NULL,
-  song_display_name TEXT NOT NULL,
-  song_file_name TEXT NOT NULL,
+  song_display_name VARCHAR(50) NOT NULL,
+  song_file_name VARCHAR(50) NOT NULL,
+  song_path TEXT NOT NULL,
   playlist_id INTEGER NOT NULL,
   FOREIGN KEY (playlist_id) REFERENCES playlist (playlist_id)
 );
