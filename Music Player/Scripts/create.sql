7b29e390c5ffdb517e1b49d56c0462a5dbff96cf
mcworkaholic <wevans302@gmail.com>
2023-03-20 23:36:47 -0500

    modified to fit SQLite syntax and slight attribute changes

--- Music Player/Scripts/create.sql
+++ Music Player/Scripts/create.sql
@@ -1,23 +1,21 @@
 CREATE TABLE user (
-  user_id NUMBER PRIMARY KEY,
-  username VARCHAR2(50) NOT NULL,
-  password VARCHAR2(50) NOT NULL
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
 );
 
 CREATE TABLE playlist (
-  playlist_id NUMBER PRIMARY KEY,
-  user_id NUMBER NOT NULL,
-  playlist_name VARCHAR2(50) NOT NULL,
-  CONSTRAINT fk_user_id FOREIGN KEY (user_id)
-    REFERENCES user (user_id)
+  playlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  playlist_name TEXT NOT NULL UNIQUE, 
+  FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
 
 CREATE TABLE song (
-  song_id NUMBER PRIMARY KEY,
-  title VARCHAR2(50) NOT NULL,
-  artist VARCHAR2(50) NOT NULL,
-  album VARCHAR2(50),
-  playlist_id NUMBER,
-  CONSTRAINT fk_playlist_id FOREIGN KEY (playlist_id)
-    REFERENCES playlist (playlist_id)
+  song_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  song_title TEXT NOT NULL,
+  song_display_name TEXT NOT NULL,
+  song_file_name TEXT NOT NULL,
+  playlist_id INTEGER NOT NULL,
+  FOREIGN KEY (playlist_id) REFERENCES playlist (playlist_id)
 );
