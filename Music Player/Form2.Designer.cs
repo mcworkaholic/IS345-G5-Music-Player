d67c159f7adbc734c9f23d426df2766cbd1c4950
mcworkaholic <wevans302@gmail.com>
2023-03-21 03:04:25 -0500

    Feat: completed login form functionality

--- Music Player/Form2.Designer.cs
+++ Music Player/Form2.Designer.cs
@@ -29,45 +29,51 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.usertextBox = new System.Windows.Forms.TextBox();
+            this.passwordtextBox = new System.Windows.Forms.TextBox();
+            this.confirmtextBox = new System.Windows.Forms.TextBox();
             this.usernameLabel = new System.Windows.Forms.Label();
             this.passwordLabel = new System.Windows.Forms.Label();
             this.confirmLabel = new System.Windows.Forms.Label();
             this.createButton = new System.Windows.Forms.Button();
             this.loginButton = new System.Windows.Forms.Button();
             this.usrerrBox = new System.Windows.Forms.PictureBox();
+            this.cfrmpwderrBox = new System.Windows.Forms.PictureBox();
+            this.confirmPanel = new System.Windows.Forms.Panel();
+            this.buttonPanel = new System.Windows.Forms.Panel();
             this.pwderrBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.usrerrBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cfrmpwderrBox)).BeginInit();
+            this.confirmPanel.SuspendLayout();
+            this.buttonPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pwderrBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // usertextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(186, 63);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.usertextBox.Location = new System.Drawing.Point(162, 63);
+            this.usertextBox.Name = "usertextBox";
+            this.usertextBox.Size = new System.Drawing.Size(100, 22);
+            this.usertextBox.TabIndex = 0;
             // 
-            // textBox2
+            // passwordtextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(186, 89);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 1;
+            this.passwordtextBox.Location = new System.Drawing.Point(162, 91);
+            this.passwordtextBox.Name = "passwordtextBox";
+            this.passwordtextBox.Size = new System.Drawing.Size(100, 22);
+            this.passwordtextBox.TabIndex = 1;
             // 
-            // textBox3
+            // confirmtextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(186, 115);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 2;
+            this.confirmtextBox.Location = new System.Drawing.Point(114, 3);
+            this.confirmtextBox.Name = "confirmtextBox";
+            this.confirmtextBox.Size = new System.Drawing.Size(100, 22);
+            this.confirmtextBox.TabIndex = 2;
             // 
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(122, 65);
+            this.usernameLabel.Location = new System.Drawing.Point(98, 66);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(61, 13);
             this.usernameLabel.TabIndex = 3;
@@ -76,7 +82,7 @@
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(124, 92);
+            this.passwordLabel.Location = new System.Drawing.Point(100, 94);
             this.passwordLabel.Name = "passwordLabel";
             this.passwordLabel.Size = new System.Drawing.Size(59, 13);
             this.passwordLabel.TabIndex = 4;
@@ -85,7 +91,7 @@
             // confirmLabel
             // 
             this.confirmLabel.AutoSize = true;
-            this.confirmLabel.Location = new System.Drawing.Point(80, 118);
+            this.confirmLabel.Location = new System.Drawing.Point(8, 8);
             this.confirmLabel.Name = "confirmLabel";
             this.confirmLabel.Size = new System.Drawing.Size(103, 13);
             this.confirmLabel.TabIndex = 5;
@@ -93,40 +99,76 @@
             // 
             // createButton
             // 
-            this.createButton.Location = new System.Drawing.Point(186, 141);
+            this.createButton.Location = new System.Drawing.Point(128, 1);
             this.createButton.Name = "createButton";
-            this.createButton.Size = new System.Drawing.Size(100, 23);
+            this.createButton.Size = new System.Drawing.Size(105, 23);
             this.createButton.TabIndex = 7;
             this.createButton.Text = "Create Profile";
             this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
             // 
             // loginButton
             // 
-            this.loginButton.Location = new System.Drawing.Point(106, 141);
+            this.loginButton.Location = new System.Drawing.Point(47, 1);
             this.loginButton.Name = "loginButton";
             this.loginButton.Size = new System.Drawing.Size(75, 23);
             this.loginButton.TabIndex = 8;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // usrerrBox
             // 
             this.usrerrBox.ErrorImage = null;
             this.usrerrBox.Image = ((System.Drawing.Image)(resources.GetObject("usrerrBox.Image")));
-            this.usrerrBox.Location = new System.Drawing.Point(292, 64);
+            this.usrerrBox.Location = new System.Drawing.Point(268, 65);
             this.usrerrBox.Name = "usrerrBox";
             this.usrerrBox.Size = new System.Drawing.Size(19, 20);
             this.usrerrBox.TabIndex = 9;
             this.usrerrBox.TabStop = false;
+            this.usrerrBox.Visible = false;
+            // 
+            // cfrmpwderrBox
+            // 
+            this.cfrmpwderrBox.Image = ((System.Drawing.Image)(resources.GetObject("cfrmpwderrBox.Image")));
+            this.cfrmpwderrBox.Location = new System.Drawing.Point(220, 5);
+            this.cfrmpwderrBox.Name = "cfrmpwderrBox";
+            this.cfrmpwderrBox.Size = new System.Drawing.Size(19, 20);
+            this.cfrmpwderrBox.TabIndex = 10;
+            this.cfrmpwderrBox.TabStop = false;
+            this.cfrmpwderrBox.Visible = false;
+            // 
+            // confirmPanel
+            // 
+            this.confirmPanel.Controls.Add(this.confirmLabel);
+            this.confirmPanel.Controls.Add(this.cfrmpwderrBox);
+            this.confirmPanel.Controls.Add(this.confirmtextBox);
+            this.confirmPanel.Location = new System.Drawing.Point(48, 116);
+            this.confirmPanel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.confirmPanel.Name = "confirmPanel";
+            this.confirmPanel.Size = new System.Drawing.Size(250, 29);
+            this.confirmPanel.TabIndex = 11;
+            this.confirmPanel.Visible = false;
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.Controls.Add(this.loginButton);
+            this.buttonPanel.Controls.Add(this.createButton);
+            this.buttonPanel.Location = new System.Drawing.Point(32, 148);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(243, 27);
+            this.buttonPanel.TabIndex = 12;
             // 
             // pwderrBox
             // 
+            this.pwderrBox.ErrorImage = null;
             this.pwderrBox.Image = ((System.Drawing.Image)(resources.GetObject("pwderrBox.Image")));
-            this.pwderrBox.Location = new System.Drawing.Point(292, 116);
+            this.pwderrBox.Location = new System.Drawing.Point(268, 93);
             this.pwderrBox.Name = "pwderrBox";
             this.pwderrBox.Size = new System.Drawing.Size(19, 20);
-            this.pwderrBox.TabIndex = 10;
+            this.pwderrBox.TabIndex = 13;
             this.pwderrBox.TabStop = false;
+            this.pwderrBox.Visible = false;
             // 
             // loginForm
             // 
@@ -134,20 +176,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(387, 208);
             this.Controls.Add(this.pwderrBox);
+            this.Controls.Add(this.buttonPanel);
+            this.Controls.Add(this.confirmPanel);
             this.Controls.Add(this.usrerrBox);
-            this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.createButton);
-            this.Controls.Add(this.confirmLabel);
             this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.usernameLabel);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passwordtextBox);
+            this.Controls.Add(this.usertextBox);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "loginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.loginForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.usrerrBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cfrmpwderrBox)).EndInit();
+            this.confirmPanel.ResumeLayout(false);
+            this.confirmPanel.PerformLayout();
+            this.buttonPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pwderrBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -156,15 +202,18 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox usertextBox;
+        private System.Windows.Forms.TextBox passwordtextBox;
+        private System.Windows.Forms.TextBox confirmtextBox;
         private System.Windows.Forms.Label usernameLabel;
         private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.Label confirmLabel;
         private System.Windows.Forms.Button createButton;
         private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.PictureBox usrerrBox;
+        private System.Windows.Forms.PictureBox cfrmpwderrBox;
+        private System.Windows.Forms.Panel confirmPanel;
+        private System.Windows.Forms.Panel buttonPanel;
         private System.Windows.Forms.PictureBox pwderrBox;
     }
 }
--- Music Player/Form2.cs
+++ Music Player/Form2.cs
@@ -1,20 +1,136 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 using System.Windows.Forms;
+using BC = BCrypt.Net.BCrypt;
 
 namespace Music_Player
 {
     public partial class loginForm : Form
     {
+        Form1 form1 = new Form1();
         public loginForm()
         {
             InitializeComponent();
+        }
+        string connectionString;
+        bool createButtonClicked = false;
+
+        // Hash a password using Bcrypt
+        public static string HashPassword(string password)
+        {
+            string hashedPassword = BC.HashPassword(password);
+            return (hashedPassword);
+        }
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+            // Set to no text.
+            passwordtextBox.Text = "";
+            // The password character is an asterisk.
+            passwordtextBox.PasswordChar = '*';
+            // The control will allow no more than 50 characters.
+            passwordtextBox.MaxLength = 50;
+            // set path to provided database
+            string workingDirectory = Environment.CurrentDirectory;
+            string dbPath = Directory.GetParent(workingDirectory).Parent.FullName + "\\Data\\users.db";
+            connectionString = $@"Data Source={dbPath};";
+            buttonPanel.Location = new System.Drawing.Point(72, 119);
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (!createButtonClicked)
+            {
+                buttonPanel.Location = new System.Drawing.Point(32, 148);
+                loginButton.Visible = false;
+                confirmPanel.Location = new System.Drawing.Point(48, 116);
+                confirmPanel.Visible = true;
+                createButton.Text = "Create";
+                createButtonClicked = true;
+            }
+            else
+            {
+                if (passwordtextBox.Text.Length > 50 || confirmtextBox.Text.Length > 50)
+                {
+                    MessageBox.Show("Password must be less than 50 characters.");
+                    cfrmpwderrBox.Visible = true;
+                    pwderrBox.Visible = true;
+                }
+                else if (usertextBox.Text.Length > 0 && passwordtextBox.Text.Length > 0)
+                {
+                    if (passwordtextBox.Text == confirmtextBox.Text)
+                    {
+                        try
+                        {
+                            // Second click: perform insert and close form
+                            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                            {
+                                connection.Open();
+                                string sql = "INSERT INTO user (username, password) VALUES (@username, @password)";
+                                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                                {
+                                    string hash = HashPassword(passwordtextBox.Text);
+                                    command.Parameters.AddWithValue("@username", usertextBox.Text);
+                                    command.Parameters.AddWithValue("@password", hash);
+                                    command.ExecuteNonQuery();
+                                }
+                            }
+                            Program.OpenForm1OnClose = true;
+                            this.Close();
+                        }
+                        catch (SQLiteException ex)
+                        {
+                            if (ex.Message.Contains("UNIQUE constraint failed: user.username"))
+                            {
+                                usrerrBox.Visible = true;
+                                MessageBox.Show("Username already exists. Please choose a different username.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        cfrmpwderrBox.Visible = true;
+                        pwderrBox.Visible = true;
+                        MessageBox.Show("Passwords must match. Please try again.");
+                    }
+                }
+            }
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = usertextBox.Text;
+
+            // Get the stored hash 
+            string storedHash = "";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT password FROM user WHERE username=@username";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            storedHash = reader.GetString(0);
+                        }
+                    }
+                }
+            }
+            if (BC.Verify(passwordtextBox.Text, storedHash))
+            {
+                Program.OpenForm1OnClose = true;
+                this.Close();
+            }
+            else
+            {
+                // Passwords don't match, so deny login
+                MessageBox.Show("Invalid username or password.");
+                usrerrBox.Visible = true;
+                cfrmpwderrBox.Visible = true;
+            }
         }
     }
 }
