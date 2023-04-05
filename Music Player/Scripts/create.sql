1e26901eaf0824ba6a6b651683d425daa86c5891
mcworkaholic <wevans302@gmail.com>
2023-04-04 21:31:19 -0500

    + extra fields for song table

--- Music Player/Scripts/create.sql
+++ Music Player/Scripts/create.sql
@@ -8,21 +8,26 @@ CREATE TABLE user (
 CREATE TABLE playlist (
   playlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
-  playlist_name VARCHAR(50) NOT NULL UNIQUE, 
+  playlist_name VARCHAR(50) NOT NULL COLLATE NOCASE UNIQUE, 
   FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
 
-CREATE TABLE playlist_song (      /*bridge */
-  ps_id INTEGER PRIMARY KEY,
+CREATE TABLE playlist_song (
+  playlist_song_id INTEGER PRIMARY KEY,
   playlist_id INTEGER NOT NULL,
   song_id INTEGER NOT NULL,
-  FOREIGN KEY (playlist_id) REFERENCES playlist(id),
-  FOREIGN KEY (song_id) REFERENCES song(id)
+  FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id),
+  FOREIGN KEY (song_id) REFERENCES song(song_id)
 );
 
 CREATE TABLE song (
   song_id INTEGER PRIMARY KEY AUTOINCREMENT,
   song_display_name VARCHAR(50) NOT NULL,
   song_file_name VARCHAR(50) NOT NULL,
-  song_path TEXT NOT NULL
+  song_path TEXT NOT NULL,
+  song_artist TEXT NOT NULL,
+  song_album TEXT,
+  song_genre TEXT,
+  song_release_year INTEGER,
+  song_duration INTEGER
 );
