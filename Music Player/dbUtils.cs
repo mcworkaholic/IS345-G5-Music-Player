8a3aab361a2db98879155e3bcc2e2e9587399c34
mcworkaholic <wevans302@gmail.com>
2023-04-04 21:34:08 -0500

    pulled db funcs from forms & added to dbUtils class

--- Music Player/dbUtils.cs
+++ Music Player/dbUtils.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Windows.Forms;
+using BC = BCrypt.Net.BCrypt;
 
 namespace Music_Player
 {
     internal class dbUtils
     {
+        // Hash a password using Bcrypt
+        public static string HashPassword(string password)
+        {
+            string hashedPassword = BC.HashPassword(password);
+            return (hashedPassword);
+        }
         public string GetStartUpFolder(string connectionString)
         {
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))
@@ -63,6 +70,59 @@ namespace Music_Player
                 }
             }
             return playlists;
+        }
+        public string GetHash(string connectionString, string username)
+        {
+            // Get the stored hash 
+            string storedHash = "";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT password, user_id FROM user WHERE username=@username";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            storedHash = reader.GetString(0);
+                            Program.user_id = reader.GetInt32(1);
+                        }
+                    }
+                }
+            }
+            return storedHash;
+        }
+        public void InsertUser(string connectionString, string username, string password)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                string hash = "";
+                string storedHash;
+                connection.Open();
+                string insert = "INSERT INTO user (username, password) VALUES (@username, @password)";
+                using (SQLiteCommand command = new SQLiteCommand(insert, connection))
+                {
+                    hash = HashPassword(password);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", hash);
+                    command.ExecuteNonQuery();
+                }
+                string select = "SELECT password, user_id FROM user WHERE username=@username";
+                using (SQLiteCommand command = new SQLiteCommand(select, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            storedHash = reader.GetString(0);
+                            Program.user_id = reader.GetInt32(1);
+                        }
+                    }
+                }
+            }
         }
         public void InsertPlaylist(ComboBox comboBox, string connectionString)
         {
