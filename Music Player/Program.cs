bbf86c396f24de6e71d9aa71b25dd816e9b7011d
mcworkaholic <wevans302@gmail.com>
2023-04-28 13:29:41 -0500

    + 2 values to be passed throughout app use

--- Music Player/Program.cs
+++ Music Player/Program.cs
@@ -6,6 +6,8 @@ namespace Music_Player
     internal static class Program
     {
         public static int user_id { get; set; }
+        public static string username { get; set; }
+        public static string sbit { get; set; }
         public static bool OpenForm1OnClose { get; set; }
         /// <summary>
         /// The main entry point for the application.
