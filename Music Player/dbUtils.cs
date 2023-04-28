6ab4b27a74bd2230eb6f149717fc90563ea04471
mcworkaholic <wevans302@gmail.com>
2023-04-27 21:25:52 -0500

    Changed from hard coded to script for db creation

--- Music Player/dbUtils.cs
+++ Music Player/dbUtils.cs
@@ -31,10 +31,9 @@ namespace Music_Player
                 using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbPath}"))
                 {
                     connection.Open();
-                    string createTables = "CREATE TABLE user, (user_id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(50) UNIQUE NOT NULL, password TEXT NOT NULL, default_startup_folder TEXT;  " +
-                        "CREATE TABLE playlist ( playlist_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, playlist_name VARCHAR(50) NOT NULL COLLATE NOCASE UNIQUE, FOREIGN KEY (user_id) REFERENCES user (user_id)); " +
-                        "CREATE TABLE playlist_song (playlist_song_id INTEGER PRIMARY KEY, playlist_id INTEGER NOT NULL, song_id INTEGER NOT NULL, FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id), FOREIGN KEY (song_id) REFERENCES song(song_id));" +
-                        "CREATE TABLE song (song_id INTEGER PRIMARY KEY AUTOINCREMENT, song_display_name VARCHAR(50) NOT NULL, song_artist TEXT NOT NULL, song_album TEXT NOT NULL, song_genre TEXT, song_release TEXT, song_duration INTEGER);";
+                    string workingDirectory = Environment.CurrentDirectory;
+                    string scriptPath = Directory.GetParent(workingDirectory).Parent.FullName + "\\Scripts\\SQL\\create-db.sql";
+                    string createTables = File.ReadAllText(scriptPath);
                     using (SQLiteCommand command = new SQLiteCommand(createTables, connection))
                     {
                         command.ExecuteNonQuery();
