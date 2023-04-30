59fc94dab23f6417e286cb30fd19db1a8436fe7a
mcworkaholic <wevans302@gmail.com>
2023-04-30 12:56:45 -0500

    + show art attribute

--- Music Player/Scripts/SQL/create-db.sql
+++ Music Player/Scripts/SQL/create-db.sql
@@ -1,20 +1,21 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   username VARCHAR(50) UNIQUE NOT NULL,
   password TEXT NOT NULL,
   default_startup_folder TEXT,
   encrypt_on_exit TEXT CHECK (encrypt_on_exit IN ('True', 'False', 'NULL')),
+  show_art TEXT CHECK (show_art IN ('True', 'False', 'NULL')),
   sbit TEXT
 );
 
-CREATE TABLE playlist (
+CREATE TABLE IF NOT EXISTS playlist (
   playlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
   playlist_name VARCHAR(50) NOT NULL COLLATE NOCASE UNIQUE, 
   FOREIGN KEY (user_id) REFERENCES user (user_id)
 );
 
-CREATE TABLE playlist_song (
+CREATE TABLE IF NOT EXISTS playlist_song (
   playlist_song_id INTEGER PRIMARY KEY AUTOINCREMENT,
   playlist_id INTEGER NOT NULL,
   song_id INTEGER NOT NULL,
@@ -22,7 +23,7 @@ CREATE TABLE playlist_song (
   FOREIGN KEY (song_id) REFERENCES song(song_id)
 );
 
-CREATE TABLE song (
+CREATE TABLE IF NOT EXISTS song (
   song_id INTEGER PRIMARY KEY AUTOINCREMENT,
   song_display_name VARCHAR(50) NOT NULL,
   song_artist TEXT NOT NULL,
