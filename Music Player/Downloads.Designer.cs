002e229309c9f65eb0be2304a012b004d478659c
mcworkaholic <wevans302@gmail.com>
2023-05-07 09:02:22 -0500

    + downloads form

---
+++ Music Player/Downloads.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Music_Player
+{
+    partial class Downloads
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Downloads));
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.closeBox = new System.Windows.Forms.PictureBox();
+            this.minimizeBox = new System.Windows.Forms.PictureBox();
+            this.downloadslistBox = new System.Windows.Forms.ListBox();
+            this.downloadLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.pastemenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimizeBox)).BeginInit();
+            this.pastemenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.topPanel.Controls.Add(this.pictureBox1);
+            this.topPanel.Controls.Add(this.closeBox);
+            this.topPanel.Controls.Add(this.minimizeBox);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(359, 26);
+            this.topPanel.TabIndex = 27;
+            this.topPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseDown);
+            this.topPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseMove);
+            this.topPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::Music_Player.Properties.Resources.login_logo;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // closeBox
+            // 
+            this.closeBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.closeBox.ErrorImage = null;
+            this.closeBox.Image = global::Music_Player.Properties.Resources.close;
+            this.closeBox.InitialImage = null;
+            this.closeBox.Location = new System.Drawing.Point(326, 3);
+            this.closeBox.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.closeBox.Name = "closeBox";
+            this.closeBox.Size = new System.Drawing.Size(26, 22);
+            this.closeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.closeBox.TabIndex = 1;
+            this.closeBox.TabStop = false;
+            this.closeBox.Click += new System.EventHandler(this.closeBox_Click);
+            this.closeBox.MouseLeave += new System.EventHandler(this.MouseLeave);
+            this.closeBox.MouseHover += new System.EventHandler(this.MouseHover);
+            // 
+            // minimizeBox
+            // 
+            this.minimizeBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.minimizeBox.ErrorImage = null;
+            this.minimizeBox.Image = global::Music_Player.Properties.Resources.minimize;
+            this.minimizeBox.InitialImage = null;
+            this.minimizeBox.Location = new System.Drawing.Point(291, 3);
+            this.minimizeBox.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.minimizeBox.Name = "minimizeBox";
+            this.minimizeBox.Size = new System.Drawing.Size(31, 22);
+            this.minimizeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.minimizeBox.TabIndex = 0;
+            this.minimizeBox.TabStop = false;
+            this.minimizeBox.Click += new System.EventHandler(this.minimizeBox_Click);
+            this.minimizeBox.MouseLeave += new System.EventHandler(this.MouseLeave);
+            this.minimizeBox.MouseHover += new System.EventHandler(this.MouseHover);
+            // 
+            // downloadslistBox
+            // 
+            this.downloadslistBox.AllowDrop = true;
+            this.downloadslistBox.FormattingEnabled = true;
+            this.downloadslistBox.HorizontalScrollbar = true;
+            this.downloadslistBox.Location = new System.Drawing.Point(6, 44);
+            this.downloadslistBox.Name = "downloadslistBox";
+            this.downloadslistBox.ScrollAlwaysVisible = true;
+            this.downloadslistBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.downloadslistBox.Size = new System.Drawing.Size(346, 355);
+            this.downloadslistBox.TabIndex = 28;
+            this.downloadslistBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.downloadslistBox_DragEnter);
+            this.downloadslistBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.downloadslistBox_MouseUp);
+            // 
+            // downloadLabel
+            // 
+            this.downloadLabel.AutoSize = true;
+            this.downloadLabel.Location = new System.Drawing.Point(3, 29);
+            this.downloadLabel.Name = "downloadLabel";
+            this.downloadLabel.Size = new System.Drawing.Size(169, 13);
+            this.downloadLabel.TabIndex = 29;
+            this.downloadLabel.Text = "Download URL\'s (SoundCloud):";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 405);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 30;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startButton.MouseLeave += new System.EventHandler(this.MouseLeave);
+            this.startButton.MouseHover += new System.EventHandler(this.MouseHover);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(93, 405);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 31;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            this.deleteButton.MouseLeave += new System.EventHandler(this.MouseLeave);
+            this.deleteButton.MouseHover += new System.EventHandler(this.MouseHover);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(174, 405);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 32;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Visible = false;
+            // 
+            // pastemenuStrip
+            // 
+            this.pastemenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pasteToolStripMenuItem});
+            this.pastemenuStrip.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.pastemenuStrip.Name = "pastemenuStrip";
+            this.pastemenuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.pastemenuStrip.ShowImageMargin = false;
+            this.pastemenuStrip.Size = new System.Drawing.Size(78, 26);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(77, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // Downloads
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 435);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.downloadLabel);
+            this.Controls.Add(this.downloadslistBox);
+            this.Controls.Add(this.topPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Downloads";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Downloads";
+            this.topPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimizeBox)).EndInit();
+            this.pastemenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox closeBox;
+        private System.Windows.Forms.PictureBox minimizeBox;
+        private System.Windows.Forms.ListBox downloadslistBox;
+        private System.Windows.Forms.Label downloadLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ContextMenuStrip pastemenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+    }
+}
---
+++ Music Player/Downloads.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player
+{
+    public partial class Downloads : Form
+    {
+        Utes utes = new Utes();
+
+        // for moving borderless form
+        private int movX, movY;
+        private bool isMoving;
+
+        private bool isDragging = false;
+
+
+        public Downloads()
+        {
+            InitializeComponent();
+        }
+
+        private void closeBox_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimizeBox_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void MouseHover(object sender, EventArgs e)
+        {
+            // hover on = 👆
+            this.Cursor = Cursors.Hand;
+        }
+        private void MouseLeave(object sender, EventArgs e)
+        {
+            // Change cursor to default when hovering away
+            this.Cursor = Cursors.Default;
+        }
+
+        private void topPanel_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            isMoving = true;
+            movX = e.X;
+            movY = e.Y;
+        }
+        private void topPanel_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (isMoving) // based on bool flag
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void downloadslistBox_DragEnter(object sender, DragEventArgs e)
+        {
+            // Paste the URL
+            downloadslistBox.Items.Add(e.Data.GetData("System.String", true).ToString());            
+        }
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            List<string> deletedItems = new List<string>();
+            foreach (var item in downloadslistBox.SelectedItems)
+            {
+                deletedItems.Add(item.ToString());
+            }
+
+            foreach (var item in deletedItems)
+            {
+                downloadslistBox.Items.Remove(item);
+            }
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetData("System.String") != null)
+            {
+                downloadslistBox.Items.Add(Clipboard.GetData("System.String"));
+            }
+        }
+
+        private void downloadslistBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                pastemenuStrip.Width = 100;
+                pastemenuStrip.Show(downloadslistBox, new Point(e.X, e.Y));
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topPanel_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            isMoving = false;
+        }
+    }
+}
